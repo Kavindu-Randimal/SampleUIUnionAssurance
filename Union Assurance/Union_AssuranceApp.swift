ce223fc4f05c1ad0faf63f7463b2051974910d1c
Kavindu-Randimal <kavi.randimal97@gmail.com>
2022-11-23 19:14:05 +0530

    Initial Commit

---
+++ Union Assurance/Union_AssuranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Union_AssuranceApp.swift
+//  Union Assurance
+//
+//  Created by Randimal Geeganage on 2022-11-23.
+//
+
+import SwiftUI
+
+@main
+struct Union_AssuranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
